7a92a5eb75e61773766b1abdc0fcd594fa4969d3
Huyen Pham <102291548+saharara@users.noreply.github.com>
2024-09-30 16:16:56 +0700

    first commit

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private id;
+    private String name, dateOfBirth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
